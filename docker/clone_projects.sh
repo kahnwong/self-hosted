cd74fab057bc84b17ccd99d73724cff9efaecc89
kahnwong <karn.wong@mailbox.org>
2021-08-29 20:25:18 +0700

    add project, test new pgp key, 2

--- docker/clone_projects.sh
+++ docker/clone_projects.sh
@@ -2,3 +2,4 @@ git clone git@github.com:iv-org/invidious.git
 git clone https://git.tt-rss.org/fox/ttrss-docker-compose.git
 git clone git@github.com:kahnwong/wirehole.git
 cd wallabag && git clone git@github.com:kahnwong/ftr-site-config.git
+git clone git@github.com:IAmStoxe/oracle-free-tier-wirehole.git
