310593f8ef7325b7cd36c2f806bdf179b59dba66
Karn Wong <karn.wong@protonmail.ch>
2021-11-04 12:09:07 +0700

    add sup3rS3cretMes5age

--- docker/clone_projects.sh
+++ docker/clone_projects.sh
@@ -3,3 +3,4 @@ git clone https://git.tt-rss.org/fox/ttrss-docker-compose.git
 git clone git@github.com:kahnwong/wirehole.git
 cd wallabag && git clone git@github.com:kahnwong/ftr-site-config.git
 git clone git@github.com:IAmStoxe/oracle-free-tier-wirehole.git
+git clone git@github.com:kahnwong/sup3rS3cretMes5age.git
