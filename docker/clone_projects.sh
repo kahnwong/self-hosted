bf49cffad4ce72e4ff8cc09aced383d1a2b0eea0
Karn Wong <karn.wong@protonmail.ch>
2021-11-15 18:57:57 +0700

    update clone_projects.sh

--- docker/clone_projects.sh
+++ docker/clone_projects.sh
@@ -4,3 +4,4 @@ git clone git@github.com:kahnwong/wirehole.git
 cd wallabag && git clone git@github.com:kahnwong/ftr-site-config.git
 git clone git@github.com:IAmStoxe/oracle-free-tier-wirehole.git
 git clone git@github.com:kahnwong/sup3rS3cretMes5age.git
+git clone git@github.com:kahnwong/dockprom.git
