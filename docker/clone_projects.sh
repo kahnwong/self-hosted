80388efdb0e92c1ca1bc1c2f7ecfddacf18b1d38
Karn Wong <karn.wong+github@mailbox.org>
2022-07-30 09:10:25 +0000

    add sourcegraph

--- docker/clone_projects.sh
+++ docker/clone_projects.sh
@@ -2,3 +2,4 @@ git clone https://git.tt-rss.org/fox/ttrss-docker-compose.git
 git clone git@github.com:kahnwong/wirehole.git
 git clone git@github.com:kahnwong/ftr-site-config.git wallabag/graby-site-config
 git clone git@github.com:kahnwong/dockprom.git
+git clone git@github.com:kahnwong/deploy-sourcegraph-docker.git
