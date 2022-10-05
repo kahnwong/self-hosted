7b71d7bbc98ecfc49262da606401ee50630ed859
Karn Wong <karn@karnwong.me>
2022-10-05 08:44:04 +0000

    update daily cleanup command

--- scripts/daily_cleanup.sh
+++ scripts/daily_cleanup.sh
@@ -2,3 +2,4 @@
 
 docker exec -t wallabag_db psql -U wallabag -d wallabag -c "DELETE FROM wallabag_entry WHERE is_archived = 't' AND is_starred = 'f';"
 docker exec -t wallabag php /var/www/wallabag/bin/console wallabag:clean-downloaded-images --env=prod
+docker exec -t wallabag php /var/www/wallabag/bin/console wallabag:clean-duplicates --env=prod
