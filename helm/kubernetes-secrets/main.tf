d01286454ec2610a64b552dd690a812ccbb4a837
kahnwong <karn@karnwong.me>
2023-01-09 13:13:17 +0700

    add k8s secrets: miniflux

--- helm/kubernetes-secrets/main.tf
+++ helm/kubernetes-secrets/main.tf
@@ -1,3 +1,13 @@
 data "sops_file" "miniflux" {
   source_file = "./secrets/miniflux.sops.yaml"
 }
+resource "kubernetes_secret" "miniflux" {
+  metadata {
+    name = "miniflux"
+  }
+
+  data = {
+    MINIFLUX_USER     = data.sops_file.miniflux.data["miniflux_user"]
+    MINIFLUX_PASSWORD = data.sops_file.miniflux.data["miniflux_password"]
+  }
+}
