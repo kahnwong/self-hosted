b16bed0c80b47a548b0e237a9ba7eb5bbb46250f
kahnwong <karn@karnwong.me>
2023-04-01 16:38:35 +0700

    add bash script to to forward opencost

--- kubernetes/grafana-port-forward.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-namespace="monitoring"
-
-pod_name=$(kubectl get pods --namespace "$namespace" | grep grafana | awk '{print $1}')
-kubectl port-forward "$pod_name" 3000:3000 --namespace "$namespace"
