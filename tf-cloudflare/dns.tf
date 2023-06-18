15e2bd6327c0d5302ace6ffbd17bade46eb1fcc1
kahnwong <karn@karnwong.me>
2023-06-18 05:34:02 +0000

    chore: remove unused DNS

--- tf-cloudflare/dns.tf
+++ tf-cloudflare/dns.tf
@@ -11,15 +11,12 @@ locals {
     "music",
     "photos",
     "podgrab",
-    "redash",
     "secrets",
-    "sourcegraph-personal",
     "sourcegraph",
     "syncthing",
     "tile",
     "ttrss",
     "wallabag",
-    "work-dashboard",
   ])
   selfhosted_non_proxied = toset([
     "jellyfin", # https://github.com/jellyfin/jellyfin-media-player/issues/174#issuecomment-1306167299
