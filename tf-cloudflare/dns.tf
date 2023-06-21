4cd1813cbe852e23aac118d752acca2b356fa725
kahnwong <karn@karnwong.me>
2023-06-21 07:27:08 +0000

    un-proxy mapserver

--- tf-cloudflare/dns.tf
+++ tf-cloudflare/dns.tf
@@ -6,7 +6,6 @@ locals {
     "api-docs",
     "budget",
     "linkding",
-    "mapserver",
     "miniflux",
     "music",
     "photos",
@@ -21,7 +20,8 @@ locals {
   ])
   selfhosted_non_proxied = toset([
     "jellyfin", # https://github.com/jellyfin/jellyfin-media-player/issues/174#issuecomment-1306167299
-    "share",    # prevent request entity too large
+    "mapserver",
+    "share", # prevent request entity too large
     "storage",
   ])
 }
