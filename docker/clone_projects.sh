05197450824628a6d95f4206269ef33eaf0100de
kahnwong <karn@karnwong.me>
2023-07-28 09:00:54 +0000

    prune projects

--- docker/clone_projects.sh
+++ docker/clone_projects.sh
@@ -1,4 +1,2 @@
 git clone https://git.tt-rss.org/fox/ttrss-docker-compose.git
-git clone git@github.com:kahnwong/wirehole.git
 git clone git@github.com:kahnwong/ftr-site-config.git /opt/wallabag/graby-site-config
-git clone git@github.com:kahnwong/dockprom.git
