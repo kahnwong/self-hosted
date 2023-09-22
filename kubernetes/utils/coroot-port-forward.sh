92ea3e0b2b766d0872cb62d1de3c8a756caccca5
kahnwong <karn@karnwong.me>
2023-09-22 14:33:54 +0700

    add makefile

--- kubernetes/utils/coroot-port-forward.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-namespace="coroot"
-kubectl port-forward -n $namespace service/coroot 8080:8080
--- kubernetes/utils/opencost-port-forward.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-namespace="opencost"
-
-pod_name=$(kubectl get pods --namespace "$namespace" | grep opencost | awk '{print $1}')
-kubectl port-forward "$pod_name" 9090:9090 --namespace "$namespace"
