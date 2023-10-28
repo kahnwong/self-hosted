547743fc0dbfa0c95054489d8bf0de5b6ead9514
kahnwong <karn@karnwong.me>
2023-10-28 07:21:54 +0000

    update wallabag cleanup pod name

--- scripts/daily_cleanup.sh
+++ scripts/daily_cleanup.sh
@@ -3,6 +3,9 @@
 WALLABAG_POD_NAME="$(kubectl get pods -l=app.kubernetes.io/name=wallabag | tail -1 | awk '{print $1}')"
 echo $WALLABAG_POD_NAME
 
+WALLABAG_STATEFULSET_POD_NAME="$(kubectl get pods -l=app.kubernetes.io/name=wallabag-statefulset | tail -1 | awk '{print $1}')"
+echo $WALLABAG_STATEFULSET_POD_NAME
+
 kubectl exec $WALLABAG_POD_NAME -c wallabag -- php /var/www/wallabag/bin/console wallabag:clean-duplicates --env=prod
 kubectl exec $WALLABAG_POD_NAME -c wallabag -- php /var/www/wallabag/bin/console wallabag:clean-downloaded-images --env=prod
-kubectl exec $WALLABAG_POD_NAME -c postgres -- psql -U wallabag -d wallabag -c "DELETE FROM wallabag_entry WHERE is_archived = 't' AND is_starred = 'f';"
+kubectl exec $WALLABAG_STATEFULSET_POD_NAME -c postgres -- psql -U wallabag -d wallabag -c "DELETE FROM wallabag_entry WHERE is_archived = 't' AND is_starred = 'f';"
