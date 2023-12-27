d78d35acced7727ca6a1d2f968c310869defccaf
Karn Wong <karn@karnwong.me>
2023-12-27 13:56:13 +0000

    refactor Caddyfile

--- scripts/caddy_reload.sh
+++ scripts/caddy_reload.sh
@@ -3,8 +3,23 @@
 cd /home/kahnwong/self-hosted/caddy/config || exit 1
 
 cat Caddyfile >all.Caddyfile
-echo -e "\n" >>all.Caddyfile
-sops -d private.Caddyfile >>all.Caddyfile
+
+{
+	echo -e "\n"
+	cat nuc.Caddyfile
+
+	echo -e "\n"
+	sops -d nuc.private.Caddyfile
+
+	echo -e "\n"
+	cat snikt.Caddyfile
+
+	echo -e "\n"
+	sops -d snikt.private.Caddyfile
+
+	echo -e "\n"
+	sops -d private.Caddyfile
+} >>all.Caddyfile
 
 sudo cp /home/kahnwong/self-hosted/caddy/config/all.Caddyfile /etc/caddy/Caddyfile
 systemctl reload caddy
