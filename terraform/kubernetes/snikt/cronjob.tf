bbccdc8ad03f62d2b77427de4e2d186307439a09
Karn Wong <karn@karnwong.me>
2024-01-24 14:15:41 +0700

    move cronjob to tf

---
+++ terraform/kubernetes/snikt/cronjob.tf
@@ -0,0 +1,14 @@
+locals {
+  jobs_family_alerts = toset([
+    "01-1-lunch-ask",
+    "01-2-lunch-order",
+    "02-1-dinner-ask-family",
+    "02-2-dinner-update-granny",
+  ])
+}
+
+resource "kubernetes_manifest" "family_alerts" {
+  for_each = local.jobs_family_alerts
+
+  manifest = yamldecode(file("../../../kubernetes/snikt/jobs-family-alerts/${each.key}.yaml"))
+}
