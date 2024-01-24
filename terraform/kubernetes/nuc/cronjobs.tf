d8b116840fa8c5ec993dbe7cf6d447db5fe2ce23
Karn Wong <karn@karnwong.me>
2024-01-24 14:26:42 +0700

    import existing cronjobs to tf

--- terraform/kubernetes/nuc/cronjobs.tf
+++ terraform/kubernetes/nuc/cronjobs.tf
@@ -1,5 +1,13 @@
 locals {
   jobs = toset([
+    "backup-linkding",
+    "backup-miniflux",
+    "backup-navidrome",
+    "backup-ntfy",
+    "backup-prune",
+    "backup-transmission",
+    "backup-wallabag-content",
+    "backup-wallabag-db",
     "email-to-epub",
   ])
 }
