2c10d6282152564bdecb640bea012ca77d92b6ee
Karn Wong <karn@karnwong.me>
2024-02-20 13:02:01 +0700

    add workspace

--- cloudflare/README.md
+++ cloudflare/README.md
@@ -28,14 +28,11 @@
 | Name | Type |
 |------|------|
 | [cloudflare_page_rule.redirect_www_to_root](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/page_rule) | resource |
+| [cloudflare_record.google_workspace_smtp](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
 | [cloudflare_record.pop_mx_send](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
 | [cloudflare_record.pop_txt_key](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
 | [cloudflare_record.pop_txt_send](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
 | [cloudflare_record.selfhosted_dns](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
-| [cloudflare_record.terraform_managed_resource_143555b19652cd46080796d693da123e](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
-| [cloudflare_record.terraform_managed_resource_453c42c73366d4a6878e501f564ba2b8](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
-| [cloudflare_record.terraform_managed_resource_46e88ec4e3f8942732a3a9c25ee4f83c](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
-| [cloudflare_record.terraform_managed_resource_989f8a3455d739ec043a6e073c70a1bb](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
 | [cloudflare_record.vaultwarden](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
 | [cloudflare_record.www_dummy](https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record) | resource |
 | [sops_file.secrets](https://registry.terraform.io/providers/carlpett/sops/1.0.0/docs/data-sources/file) | data source |
--- cloudflare/dns_email.tf
+++ cloudflare/dns_email.tf
@@ -1,47 +1,68 @@
-resource "cloudflare_record" "terraform_managed_resource_143555b19652cd46080796d693da123e" {
-  name     = "karnwong.me"
-  priority = 10
+#resource "cloudflare_record" "terraform_managed_resource_143555b19652cd46080796d693da123e" {
+#  name     = "karnwong.me"
+#  priority = 10
+#  proxied  = false
+#  ttl      = 1
+#  type     = "MX"
+#  value    = data.sops_file.secrets.data["MAILBOX_MX2"]
+#  zone_id  = local.cloudflare_zone_id
+#}
+#
+#resource "cloudflare_record" "terraform_managed_resource_989f8a3455d739ec043a6e073c70a1bb" {
+#  name     = "karnwong.me"
+#  priority = 10
+#  proxied  = false
+#  ttl      = 1
+#  type     = "MX"
+#  value    = data.sops_file.secrets.data["MAILBOX_MX1"]
+#  zone_id  = local.cloudflare_zone_id
+#}
+#
+#resource "cloudflare_record" "terraform_managed_resource_46e88ec4e3f8942732a3a9c25ee4f83c" {
+#  name     = "karnwong.me"
+#  priority = 20
+#  proxied  = false
+#  ttl      = 1
+#  type     = "MX"
+#  value    = data.sops_file.secrets.data["MAILBOX_MX3"]
+#  zone_id  = local.cloudflare_zone_id
+#}
+#
+#resource "cloudflare_record" "terraform_managed_resource_453c42c73366d4a6878e501f564ba2b8" {
+#  name     = "_autodiscover._tcp"
+#  priority = 0
+#  proxied  = false
+#  ttl      = 1
+#  type     = "SRV"
+#  zone_id  = local.cloudflare_zone_id
+#  data {
+#    name     = "karnwong.me"
+#    port     = 443
+#    priority = 0
+#    proto    = "_tcp"
+#    service  = "_autodiscover"
+#    target   = data.sops_file.secrets.data["MAILBOX_AUTODISCOVER_TARGET"]
+#    weight   = 0
+#  }
+#}
+
+
+## email alias (already verified)
+#resource "cloudflare_record" "google_workspace_alias" {
+#  name    = "@"
+#  proxied = false
+#  ttl     = 1
+#  type    = "TXT"
+#  value   = data.sops_file.secrets.data["GOOGLE_WORKSPACE_ALIAS"]
+#  zone_id = local.cloudflare_zone_id
+#}
+
+resource "cloudflare_record" "google_workspace_smtp" {
+  name     = "@"
+  priority = 1
   proxied  = false
   ttl      = 1
   type     = "MX"
-  value    = data.sops_file.secrets.data["MAILBOX_MX2"]
+  value    = "smtp.google.com"
   zone_id  = local.cloudflare_zone_id
-}
-
-resource "cloudflare_record" "terraform_managed_resource_989f8a3455d739ec043a6e073c70a1bb" {
-  name     = "karnwong.me"
-  priority = 10
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = data.sops_file.secrets.data["MAILBOX_MX1"]
-  zone_id  = local.cloudflare_zone_id
-}
-
-resource "cloudflare_record" "terraform_managed_resource_46e88ec4e3f8942732a3a9c25ee4f83c" {
-  name     = "karnwong.me"
-  priority = 20
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  value    = data.sops_file.secrets.data["MAILBOX_MX3"]
-  zone_id  = local.cloudflare_zone_id
-}
-
-resource "cloudflare_record" "terraform_managed_resource_453c42c73366d4a6878e501f564ba2b8" {
-  name     = "_autodiscover._tcp"
-  priority = 0
-  proxied  = false
-  ttl      = 1
-  type     = "SRV"
-  zone_id  = local.cloudflare_zone_id
-  data {
-    name     = "karnwong.me"
-    port     = 443
-    priority = 0
-    proto    = "_tcp"
-    service  = "_autodiscover"
-    target   = data.sops_file.secrets.data["MAILBOX_AUTODISCOVER_TARGET"]
-    weight   = 0
-  }
 }
