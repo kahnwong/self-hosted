29093ad1a98bd04fb816e7940b6b88fb13a209e0
Karn Wong <karn@karnwong.me>
2024-03-13 23:23:55 +0700

    add livegrep restart

--- scripts/daily_cleanup.sh
+++ scripts/daily_cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$PATH:/home/kahnwong/.nix-profile/bin
+
 # ------- wallabag -------
 kubectl config use-context snikt
 
@@ -12,3 +14,7 @@ echo $WALLABAG_STATEFULSET_POD_NAME
 kubectl exec $WALLABAG_POD_NAME -c wallabag -- php /var/www/wallabag/bin/console wallabag:clean-duplicates --env=prod
 kubectl exec $WALLABAG_POD_NAME -c wallabag -- php /var/www/wallabag/bin/console wallabag:clean-downloaded-images --env=prod
 kubectl exec $WALLABAG_STATEFULSET_POD_NAME -c postgres -- psql -U wallabag -d wallabag -c "DELETE FROM wallabag_entry WHERE is_archived = 't' AND is_starred = 'f';"
+
+# ------- livegrep -------
+kubectl config use-context snikt
+kubectl rollout restart deploy livegrep
