6ebaa797a75f2d8e3a5fa2aaabc9c8b20aa17e22
Karn Wong <karn@karnwong.me>
2024-07-13 17:08:02 +0700

    use secrets for forgejo-postgres

--- kubernetes/clusters/snikt/secrets.tf
+++ kubernetes/clusters/snikt/secrets.tf
@@ -2,7 +2,7 @@ locals {
   secrets = tomap({
     default            = []
     immich             = ["immich", "immich-machine-learning", "immich-postgres"]
-    infrastructure     = ["forgejo", "minio", "mlflow", "mlflow-postgres"]
+    infrastructure     = ["forgejo", "forgejo-postgres", "minio", "mlflow", "mlflow-postgres"]
     miniflux           = ["miniflux"]
     jobs               = ["backup", "ddns", "water-cut-notify", "immich-postgres"]
     jobs-family-alerts = ["family-alerts"]
