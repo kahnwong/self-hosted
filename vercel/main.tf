71e1f35549d3b0f167f4db9292b564018c926da9
Karn Wong <karn@karnwong.me>
2024-07-22 23:55:12 +0700

    adjust vercel project config

--- vercel/main.tf
+++ vercel/main.tf
@@ -1,7 +1,5 @@
 resource "vercel_project" "shouldideploytoday" {
   name = "shouldideploytoday"
 
-  install_command  = "npm install"
-  build_command    = "npm run build"
-  output_directory = ".next"
+  framework = "nextjs"
 }
