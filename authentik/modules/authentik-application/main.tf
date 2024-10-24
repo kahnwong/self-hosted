6c81271c3ba9622c689c142e7080bb21c034aecf
Karn Wong <karn@karnwong.me>
2024-10-24 17:47:37 +0700

    authentik rename provider

--- authentik/modules/authentik-application/main.tf
+++ authentik/modules/authentik-application/main.tf
@@ -3,7 +3,7 @@ data "authentik_flow" "default-authorization-flow" {
 }
 
 resource "authentik_provider_proxy" "this" {
-  name               = var.application_name
+  name               = "Provider for ${var.application_name}"
   external_host      = "https://${var.application_name}.karnwong.me"
   mode               = "forward_single"
   authorization_flow = data.authentik_flow.default-authorization-flow.id
