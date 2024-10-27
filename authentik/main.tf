fe9ddb23583724afb4083c5acad626e86c71a563
Karn Wong <karn@karnwong.me>
2024-10-27 19:37:20 +0700

    authentik: add mlflow

--- authentik/main.tf
+++ authentik/main.tf
@@ -19,6 +19,7 @@ locals {
     "linkding",
     "livegrep",
     "gatus",
+    "mlflow",
     "podgrab"
   ])
 }
@@ -42,46 +43,4 @@ module "application_proxy" {
   source            = "./modules/authentik-application-proxy"
   authentik_flow_id = data.authentik_flow.default-authorization-flow.id
   application_name  = each.key
-}
-
-## obtain from browser inspector
-# import {
-#   id = "xxxxx-xxxx-xxx-xxx-xxxx"
-#   to = authentik_service_connection_kubernetes.local
-# }
-resource "authentik_service_connection_kubernetes" "local" {
-  name  = "Local Kubernetes Cluster"
-  local = true
-}
-
-## obtain from browser inspector
-# import {
-#   id = "xxxxx-xxxx-xxx-xxx-xxxx"
-#   to = authentik_outpost.proxy_outpost
-# }
-resource "authentik_outpost" "proxy_outpost" {
-  name               = "authentik Embedded Outpost"
-  protocol_providers = [for app in module.application_proxy : app.provider_proxy_id]
-  config = jsonencode({
-    authentik_host                 = format(var.authentik_host)
-    authentik_host_browser         = ""
-    authentik_host_insecure        = false
-    container_image                = null
-    docker_labels                  = null
-    docker_map_ports               = true
-    docker_network                 = null
-    kubernetes_disabled_components = []
-    kubernetes_image_pull_secrets  = []
-    kubernetes_ingress_annotations = {}
-    kubernetes_ingress_class_name  = null
-    kubernetes_ingress_secret_name = "authentik-outpost-tls"
-    kubernetes_json_patches        = null
-    kubernetes_namespace           = "authentik"
-    kubernetes_replicas            = 1
-    kubernetes_service_type        = "ClusterIP"
-    log_level                      = "info"
-    object_naming_template         = "ak-outpost-%(name)s"
-    refresh_interval               = "minutes=5"
-  })
-  service_connection = authentik_service_connection_kubernetes.local.id
 }
