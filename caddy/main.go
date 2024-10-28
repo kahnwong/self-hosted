127992c3de5dd893f7ff7d6757433524fe3353c1
Karn Wong <karn@karnwong.me>
2024-10-28 10:20:19 +0700

    qa-discord-bot: switch to qa-api-rs

--- caddy/main.go
+++ caddy/main.go
@@ -6,6 +6,59 @@ import (
 	"sort"
 )
 
+func main() {
+	services := map[string]int{
+		"audiobookshelf":   30027,
+		"authentik":        30047,
+		"books":            30032,
+		"console.minio":    30021,
+		"excalidraw":       30034,
+		"gist":             30039,
+		"git":              30026,
+		"go":               30042,
+		"harbor":           30500,
+		"immich":           30030,
+		"jellyfin":         30003,
+		"memos":            30031,
+		"miniflux":         30007,
+		"minio":            30020,
+		"music":            30006,
+		"ntfy":             30022,
+		"pdf":              30040,
+		"plausible":        30044,
+		"qa-api":           30045,
+		"rustpad":          30019,
+		"secrets":          30025,
+		"share":            30017,
+		"subsonic-widgets": 30038,
+		"syncthing":        8384,
+		"thai-tech-cal":    30046,
+		"wakapi":           30041,
+	}
+	servicesForwardAuth := map[string]int{
+		"console.mlflow": 30037,
+		"dashy":          30023,
+		"gatus":          30029,
+		"linkding":       30005,
+		"livegrep":       30033,
+		"podgrab":        30004,
+	}
+
+	// generate config
+	config := generateConfig(services)
+	configForwardAuth := generateConfigForwardAuth(servicesForwardAuth)
+
+	configAll := config + configForwardAuth
+	fmt.Println(configAll)
+
+	// write to file
+	err := os.WriteFile("./config/Caddyfile", []byte(configAll), 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Caddyfile configured")
+}
+
 func generateConfig(services map[string]int) string {
 	config := ""
 
@@ -54,58 +107,4 @@ func generateConfigForwardAuth(services map[string]int) string {
 	}
 
 	return config
-}
-
-func main() {
-	services := map[string]int{
-		"audiobookshelf":   30027,
-		"authentik":        30047,
-		"books":            30032,
-		"console.minio":    30021,
-		"excalidraw":       30034,
-		"gist":             30039,
-		"git":              30026,
-		"go":               30042,
-		"harbor":           30500,
-		"immich":           30030,
-		"jellyfin":         30003,
-		"memos":            30031,
-		"miniflux":         30007,
-		"minio":            30020,
-		"music":            30006,
-		"ntfy":             30022,
-		"pdf":              30040,
-		"plausible":        30044,
-		"qa-api":           30043,
-		"rustpad":          30019,
-		"secrets":          30025,
-		"share":            30017,
-		"subsonic-widgets": 30038,
-		"syncthing":        8384,
-		"thai-tech-cal":    30046,
-		"wakapi":           30041,
-	}
-	servicesForwardAuth := map[string]int{
-		"console.mlflow": 30037,
-		"dashy":          30023,
-		"gatus":          30029,
-		"linkding":       30005,
-		"livegrep":       30033,
-		"podgrab":        30004,
-	}
-
-	// generate config
-	config := generateConfig(services)
-	configForwardAuth := generateConfigForwardAuth(servicesForwardAuth)
-
-	configAll := config + configForwardAuth
-	fmt.Println(configAll)
-
-	// write to file
-	err := os.WriteFile("./config/Caddyfile", []byte(configAll), 0644)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Caddyfile configured")
-
 }
