5c521ba7db56a69ec1a2809b04dc38fc567da339
Karn Wong <karn@karnwong.me>
2025-07-29 21:12:06 +0700

    authentik: bump to 2025.6.4

--- kubernetes/clusters/deployments/deployments.tf
+++ kubernetes/clusters/deployments/deployments.tf
@@ -193,7 +193,7 @@ resource "helm_release" "authentik" {
   name       = "authentik"
   namespace  = "authentik"
   repository = "https://charts.goauthentik.io"
-  version    = "2025.6.3"
+  version    = "2025.6.4"
   chart      = "authentik"
 
   values = [
