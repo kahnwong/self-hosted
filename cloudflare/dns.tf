5a1b912c970f43628c0d28381575c5a450ebfef1
Karn Wong <karn@karnwong.me>
2025-07-31 12:18:09 +0700

    cloudflare: refactor dns

--- cloudflare/dns.tf
+++ cloudflare/dns.tf
@@ -64,11 +64,38 @@ locals {
     "warpgate",
     # "share", # prevent request entity too large; migrated to oracle
   ]), var.private_dns)
+
+  gcp_proxied = toset([
+    "vaultwarden",
+    "umami",
+  ])
+  gcp_non_proxied = toset([])
+
+  oracle_proxied = toset([
+    "anisette",
+    "kutt",
+    "pairdrop",
+    "share",
+  ])
+  oracle_non_proxied = toset([
+    "croc",
+  ])
 }
 locals {
+  # on-prem
   proxied_dict     = { for name in local.selfhosted_proxied : name => true }
   non_proxied_dict = { for name in local.selfhosted_non_proxied : name => false }
   selfhosted_dns   = merge(local.proxied_dict, local.non_proxied_dict)
+
+  # gcp
+  gcp_proxied_dict     = { for name in local.gcp_proxied : name => true }
+  gcp_non_proxied_dict = { for name in local.gcp_non_proxied : name => false }
+  gcp_dns              = merge(local.gcp_proxied_dict, local.gcp_non_proxied_dict)
+
+  # oracle
+  oracle_proxied_dict     = { for name in local.oracle_proxied : name => true }
+  oracle_non_proxied_dict = { for name in local.oracle_non_proxied : name => false }
+  oracle_dns              = merge(local.oracle_proxied_dict, local.oracle_non_proxied_dict)
 }
 
 resource "cloudflare_dns_record" "github_pages_dns" {
@@ -101,69 +128,25 @@ resource "cloudflare_dns_record" "selfhosted_dns" {
   zone_id  = var.cloudflare_zone_id
 }
 
-resource "cloudflare_dns_record" "vaultwarden" {
-  name    = "vaultwarden.karnwong.me"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  content = data.sops_file.secrets.data["GCP_VM_IP"]
-  zone_id = var.cloudflare_zone_id
+resource "cloudflare_dns_record" "gcp_dns" {
+  for_each = local.gcp_dns
+  name     = "${each.key}.karnwong.me"
+  proxied  = each.value
+  ttl      = 1
+  type     = "A"
+  content  = data.sops_file.secrets.data["GCP_VM_IP"]
+  zone_id  = var.cloudflare_zone_id
 }
 
-resource "cloudflare_dns_record" "umami" {
-  name    = "umami.karnwong.me"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  content = data.sops_file.secrets.data["GCP_VM_IP"]
-  zone_id = var.cloudflare_zone_id
+resource "cloudflare_dns_record" "oracle_dns" {
+  for_each = local.oracle_dns
+  name     = "${each.key}.karnwong.me"
+  proxied  = each.value
+  ttl      = 1
+  type     = "A"
+  content  = data.sops_file.secrets.data["ORACLE_VM_IP"]
+  zone_id  = var.cloudflare_zone_id
 }
-
-resource "cloudflare_dns_record" "pairdrop" {
-  name    = "pairdrop.karnwong.me"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  content = data.sops_file.secrets.data["ORACLE_VM_IP"]
-  zone_id = var.cloudflare_zone_id
-}
-
-resource "cloudflare_dns_record" "anisette" {
-  name    = "anisette.karnwong.me"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  content = data.sops_file.secrets.data["ORACLE_VM_IP"]
-  zone_id = var.cloudflare_zone_id
-}
-
-resource "cloudflare_dns_record" "kutt" {
-  name    = "kutt.karnwong.me"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  content = data.sops_file.secrets.data["ORACLE_VM_IP"]
-  zone_id = var.cloudflare_zone_id
-}
-
-resource "cloudflare_dns_record" "picoshare" {
-  name    = "share.karnwong.me"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  content = data.sops_file.secrets.data["ORACLE_VM_IP"]
-  zone_id = var.cloudflare_zone_id
-}
-
-resource "cloudflare_dns_record" "croc" {
-  name    = "croc.karnwong.me"
-  proxied = false
-  ttl     = 1
-  type    = "A"
-  content = data.sops_file.secrets.data["ORACLE_VM_IP"]
-  zone_id = var.cloudflare_zone_id
-}
-
 
 
 # need for redirection
