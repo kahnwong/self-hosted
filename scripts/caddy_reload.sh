821bfa69bf5593c722f14fe544be413873903f66
Karn Wong <karn@karnwong.me>
2025-10-21 13:31:03 +0700

    caddy: add crowdsec

--- scripts/caddy_reload.sh
+++ scripts/caddy_reload.sh
@@ -4,10 +4,10 @@ git fetch
 git reset origin/master --hard
 cd /home/ubuntu/self-hosted/caddy/config || exit 1
 
-cat Caddyfile >all.Caddyfile
-cat misc.Caddyfile >>all.Caddyfile
-
 {
+	sops -d header.sops.Caddyfile
+	cat Caddyfile
+	cat misc.Caddyfile
 	echo -e "\n"
 	sops -d private.sops.Caddyfile
 } >>all.Caddyfile
