1092c9e5e74e97a22b9549255e6fe30f45e528f5
Karn Wong <karn@karnwong.me>
2026-01-02 13:25:50 +0700

    github: migrate to gcs backend

--- github/backend.tf
+++ github/backend.tf
@@ -1,10 +1,6 @@
 terraform {
-  cloud {
-    organization = "fringe-division"
-    hostname     = "app.terraform.io"
-
-    workspaces {
-      tags = ["github"]
-    }
+  backend "gcs" {
+    bucket = "kahnwong-tfstate"
+    prefix = "terraform/github"
   }
 }
