90945221cd69166b792339dca68c7f4934fc2fa3
Karn Wong <karn@karnwong.me>
2026-01-02 13:33:32 +0700

    k8s-nuc-pvc: migrate to gcs backend

--- kubernetes/clusters/nuc/pvc/backend.tf
+++ kubernetes/clusters/nuc/pvc/backend.tf
@@ -1,10 +1,6 @@
 terraform {
-  cloud {
-    organization = "fringe-division"
-    hostname     = "app.terraform.io"
-
-    workspaces {
-      tags = ["kubernetes-pvc"]
-    }
+  backend "gcs" {
+    bucket = "kahnwong-tfstate"
+    prefix = "terraform/kubernetes/nuc/pvc"
   }
 }
