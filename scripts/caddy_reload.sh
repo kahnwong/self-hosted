19baa1fb62c265e45d4d9291dfaa77e6d8a24283
Karn Wong <karn@karnwong.me>
2026-04-12 17:37:24 +0700

    caddy: use API to update config

--- scripts/caddy_reload.sh
+++ scripts/caddy_reload.sh
@@ -12,5 +12,10 @@ cd /home/ubuntu/self-hosted/caddy/config || exit 1
 	sops -d fd.private.sops.Caddyfile
 } >all.Caddyfile
 
-sudo cp /home/ubuntu/self-hosted/caddy/config/all.Caddyfile /etc/caddy/Caddyfile
-systemctl reload caddy
+sudo cp /home/ubuntu/self-hosted/caddy/config/all.Caddyfile /etc/caddy/Caddyfile # write latest config, useful when debugging
+# systemctl reload caddy # legacy
+
+caddy fmt --overwrite all.Caddyfile
+curl localhost:2019/load \
+	-H "Content-Type: text/caddyfile" \
+	--data-binary @all.Caddyfile
