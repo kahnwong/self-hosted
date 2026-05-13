3e57f347245b334f2a8d3421097d11ca52bfcac3
Karn Wong <karn@karnwong.me>
2026-05-13 15:47:49 +0700

    cloudnative-pg: add setup

---
+++ kubernetes/setup/cloudnative-pg/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl apply --server-side -f \
+  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.29/releases/cnpg-1.29.1.yaml
--- kubernetes/setup/greenkube/cleanup.sh
+++
@@ -1,2 +0,0 @@
-export NAMESPACE=greenkube
-kubectl get ns $NAMESPACE -o json | jq '.spec.finalizers = []' | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
