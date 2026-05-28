846093e569bee60f9dd7472d5bb5354580430abd
Karn Wong <karn@karnwong.me>
2026-05-28 19:29:42 +0700

    lemonade: move to docs

---
+++ systemd/nuc/lemonade/README.md
@@ -0,0 +1,5 @@
+# Lemonade
+
+- web app: http://localhost:13305
+- set host: `lemonade config set host=0.0.0.0`
+- restart: `sudo systemctl restart lemond`
--- systemd/nuc/lemonade/setup.sh
+++ systemd/nuc/lemonade/setup.sh
@@ -5,7 +5,3 @@ sudo apt install -y lemonade-server lemonade-desktop
 
 lemonade backends install llamacpp:vulkan
 lemonade pull Bonsai-1.7B-gguf
-
-# web app: http://localhost:13305
-# set host: lemonade config set host=0.0.0.0
-# restart: sudo systemctl restart lemond
