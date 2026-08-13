c8d9f98c4947ca98d3bedd0df538b5cadd681aff
Karn Wong <karn@karnwong.me>
2026-08-13 20:16:39 +0700

    agent-sandbox: add sample workload

--- kubernetes/setup/agent-sandbox/README.md
+++ kubernetes/setup/agent-sandbox/README.md
@@ -1,3 +1,13 @@
 # Agent Sandbox
 
 <https://agent-sandbox.sigs.k8s.io/docs/getting_started/overview/>
+
+## Setup
+
+`./setup.sh`
+
+## Usage
+
+```bash
+kubectl apply -f sandbox.yaml
+```
--- kubernetes/setup/agent-sandbox/setup.sh
+++ kubernetes/setup/agent-sandbox/setup.sh
@@ -3,3 +3,6 @@
 #VERSION=$(curl https://api.github.com/repos/kubernetes-sigs/agent-sandbox/releases/latest | jq -r '.tag_name')
 VERSION="v0.5.4"
 kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/sandbox-with-extensions.yaml
+
+# create namespace for sandboxes
+kubectl create namespace agent-sandboxes
