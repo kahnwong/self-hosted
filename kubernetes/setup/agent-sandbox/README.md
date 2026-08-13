cee9cafff1468812cb888dcd56aea81b949dd0c6
Karn Wong <karn@karnwong.me>
2026-08-13 19:39:57 +0700

    agent-sandbox: init

---
+++ kubernetes/setup/agent-sandbox/README.md
@@ -0,0 +1,3 @@
+# Agent Sandbox
+
+<https://agent-sandbox.sigs.k8s.io/docs/getting_started/overview/>
---
+++ kubernetes/setup/agent-sandbox/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#VERSION=$(curl https://api.github.com/repos/kubernetes-sigs/agent-sandbox/releases/latest | jq -r '.tag_name')
+VERSION="v0.5.4"
+kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/sandbox-with-extensions.yaml
