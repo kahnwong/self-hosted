937b5a0c0c6de73345e230ecee88be2ee67e9c6c
Karn Wong <karn@karnwong.me>
2026-09-18 17:28:51 +0700

    agent-sandbox: bump to v1.0.3

--- kubernetes/setup/agent-sandbox/setup.sh
+++ kubernetes/setup/agent-sandbox/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #VERSION=$(curl https://api.github.com/repos/kubernetes-sigs/agent-sandbox/releases/latest | jq -r '.tag_name')
-VERSION="v1.0.0"
+VERSION="v1.0.3"
 kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/sandbox-with-extensions.yaml
